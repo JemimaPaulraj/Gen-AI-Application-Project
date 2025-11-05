5e231da24f4ce27695cff5a9f7803bb40bc0abcf
Jemima Paulraj <75728827+JemimaPaulraj@users.noreply.github.com>
2025-11-05 12:28:13 -0500

    Update 5_Image Generator.py

--- pages/5_Image Generator.py
+++ pages/5_Image Generator.py
@@ -1,18 +1,9 @@
-# ==========================================================
-# 🖼️ Image Generator (Streamlit Cloud Safe - CPU Only)
-# ==========================================================
-import os
-from dotenv import load_dotenv
-load_dotenv()
-os.environ["STREAMLIT_WATCH_FILES"] = "false"
-
-import torch
+# Import Necessary packages
+import torch # Imports PyTorch library for tensor operations and GPU acceleration
 import streamlit as st
 import matplotlib.pyplot as plt
-from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionPipeline  # it comes from Hugging Face diffusers library to generate image to text.
 
-# API keys
-HF_TOKEN = os.getenv("HF_TOKEN")
 #---------------------------------------------------------------------------------------------------------------
 st.set_page_config(page_title=" Image Generator ", layout="wide")
 # Styling the heading
@@ -57,14 +48,14 @@ st.markdown("<hr style='margin: 15px 0;'>", unsafe_allow_html=True)
 # Get the input model name from user
 col1, col2, col3= st.columns([1, 1, 1])
 with col1:
-    model = st.selectbox("***Model***", ["Segmind Small-SD"])
+    model = st.selectbox("***Model***", ["Stable Diffusion v1.5","Dreamlike Photoreal 2.0"])
 
 
 with col2:
     Image_Size = st.selectbox("***Image Size***", ["256x256","512x512"])
 
 with col3:
-    Number_of_Images = st.slider("***Number of Images***", min_value=1, max_value=2, value=1, step=1)
+    Number_of_Images = st.slider("***Number of Images***", min_value=1, max_value=3, value=1, step=1)
 
 #spacer 
 st.markdown("<hr style='margin: 15px 0;'>", unsafe_allow_html=True)
@@ -77,24 +68,21 @@ with col1:
 with col2:
     for _ in range(5):
         st.write("")
-    Generate_clicked = st.button("Generate")
+    summarize_clicked = st.button("Generate")
 
 model_map = {
-    "Segmind Small-SD": "segmind/small-sd" }
+    "Stable Diffusion v1.5": "runwayml/stable-diffusion-v1-5",
+    "Dreamlike Photoreal 2.0": "dreamlike-art/dreamlike-photoreal-2.0",
+}
 #---------------------------------------------------------------------------------------------------------------
+# Cache Model Loading for faster reloads
 @st.cache_resource
-def load_pipeline(model_id):
-    try:
-        pipe = StableDiffusionPipeline.from_pretrained(
-            model_id,
-            torch_dtype=torch.float32,
-            token=HF_TOKEN,
-            low_cpu_mem_usage=True
-        )
-        return pipe.to("cpu")
-    except Exception as e:
-        st.error(f"⚠️ Failed to load model due to memory overload in streamlit cloud..")
-        return None
+def load_pipeline(model_id, device):
+    pipe = StableDiffusionPipeline.from_pretrained(
+        model_id,
+        torch_dtype = torch.float16 if device=="cuda" else torch.float32
+    )
+    return pipe.to(device)
 
 #---------------------------------------------------------------------------------------------------------------
                                 # Model Pipeline
@@ -115,13 +103,15 @@ def generate_images(pipe, prompt, params):
         st.image(small_img, caption="Generated Image", use_container_width=False)
 
 # generating images
-if Generate_clicked and User_Prompt.strip():
+if summarize_clicked and User_Prompt.strip():
     with st.spinner("Generating Images, It may take some time..."):
+        st.error("⚠️ Please run image generation locally with GPU. Stable Diffusion models require significant memory and may cause memory overload on Streamlit Cloud.") # remove this line when running in local
         model_id = model_map[model]
-        pipe = load_pipeline(model_id)
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        st.write(f"Running on: {device.upper()}")
+        pipe = load_pipeline(model_id, device)
         params = {
-            "num_inference_steps": 30,
-            "guidance_scale": 7.5, 
+            "num_inference_steps": 50,
             "num_images_per_prompt": Number_of_Images
         }
         generate_images(pipe, User_Prompt, params)
